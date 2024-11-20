ff24c1b0a10dbde92327dc9f3307f170db34b9eb
gabycode <86427446+gabycode@users.noreply.github.com>
2024-11-20 17:18:31 -0400

    add: cors

--- main.ts
+++ main.ts
@@ -1,10 +1,12 @@
 import { PrismaClient } from "@prisma/client";
 import express from "express";
 import morgan from "morgan";
+import cors from "cors";
 
 const app = express();
 app.use(express.json());
 app.use(morgan("combined"));
+app.use(cors());
 
 export const prisma = new PrismaClient();
 
