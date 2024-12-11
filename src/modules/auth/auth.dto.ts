88f6a4262fe87ea8996f6dff9ca08688806fc28e
gabycode <86427446+gabycode@users.noreply.github.com>
2024-12-11 11:08:53 -0400

    add: error handling on authentication

--- src/modules/auth/auth.dto.ts
+++ src/modules/auth/auth.dto.ts
@@ -2,16 +2,28 @@ import { z } from "zod";
 
 // Validación para creación de usuario
 export const UserRegisterSchema = z.object({
-  name: z.string().min(2, "Name is required"),
-  lastName: z.string().min(2, "Description is required"),
-  password: z.string().min(8, "Password is required").optional(),
-  email: z.string().min(2, "Description is required"),
+  name: z
+    .string()
+    .min(2, "Name is required")
+    .regex(/^[a-zA-Z\s]+$/, "Name must only contain letters and spaces"),
+  lastName: z
+    .string()
+    .min(2, "Last name is required")
+    .regex(/^[a-zA-Z\s]+$/, "Last name must only contain letters and spaces"),
+  password: z.string().min(8, "Password must be at least 8 characters long"),
+  email: z
+    .string()
+    .email("Invalid email format") // Valida que sea un correo electrónico válido
+    .regex(
+      /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+      "Email must only contain valid email characters",
+    ),
 });
 
 // Validación para actualización de usuario
 export const UserLoginSchema = z.object({
-  email: z.string().min(2, "Email is required").optional(),
-  password: z.string().min(8, "Password is required").optional(),
+  email: z.string().min(2, "Email is required"),
+  password: z.string().min(8, "Password is required"),
 });
 
 // Exporta los tipos para usarlos en el resto de la app si es necesario
